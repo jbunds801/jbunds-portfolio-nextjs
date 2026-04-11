f7888ccf3d92051daccda5024c99aaa02c5fa63b
Jessica <jbunds@proton.me>
2026-04-10 18:51:02 -0600

    updated footer. Made text transparent.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='fixed bottom-0 right-0 w-full'>
-            <p className='text-xs flex font-extralight justify-end px-4 pb-2'>© Jessica Bundy 2026</p>
+            <p className='text-xs flex font-extralight text-current/50 justify-end px-4 pb-2'>© Jessica Bundy 2026</p>
         </div>
     )
 }
